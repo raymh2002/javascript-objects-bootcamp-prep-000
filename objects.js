e07e08a723997029487061b4d52fde65c66bafaa
Raymond Harvey <raymh2002@aol.com>
2018-01-20 21:40:43 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,6 +6,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  ([playlist]).(artistName).delete;
+  (playlist).(artist) = ;
   return playlist;
 }
